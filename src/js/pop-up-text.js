c38c71930f03925e5b1b9a5a249cf3371a5f10b0
Dmytro Stetsenko <97848708+stets-js@users.noreply.github.com>
2023-02-04 16:40:15 +0200

    update about us section
    add animation for read more btn

--- src/js/pop-up-text.js
+++ src/js/pop-up-text.js
@@ -1,8 +1,28 @@
 import $ from 'jquery';
 
 $('[data-btn-more]').on('click', function () {
+  $("section.about-us").animate({
+    height: '700px'
+  }, 800, function(){
+    $(this).find('[data-text-less]').removeClass('is-hidden');
+  $(this).find('[data-btn-less]').removeClass('is-hidden');
+  });
   $(this).siblings('[data-text]').toggle('slow');
+  $(this).find('[data-text-more]').addClass('is-hidden');
+    $(this).find('[data-btn-more]').addClass('is-hidden');
+  //$("section.about-us").toggleClass('long-height-section');
+});
 
-  $(this).find('[data-text-more]').toggleClass('is-hidden');
-  $(this).find('[data-text-less]').toggleClass('is-hidden');
+$('[data-btn-less]').on('click', function () {
+  $("section.about-us").animate({
+    height: '500px'
+  }, 800, function () {
+    
+  $(this).find('[data-text-more]').removeClass('is-hidden');
+  $(this).find('[data-btn-more]').removeClass('is-hidden');
+  });
+  $(this).siblings('[data-text]').toggle('slow');
+  $(this).find('[data-text-less]').addClass('is-hidden');
+  $(this).find('[data-btn-less]').addClass('is-hidden');
+  //$("section.about-us").toggleClass('long-height-section');
 });
