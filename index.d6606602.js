18869e3ad59c0909595ef2af9f05b3e5e18f81b6
stets-js <97848708+stets-js@users.noreply.github.com>
2023-03-02 16:36:08 +0000

    Deploying to gh-pages from @ stets-js/sap_landing@c33c35d0cf62a703c9b1043479a05207ec065b5a 🚀

---
+++ index.d6606602.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,centeredSlidesBounds:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{480:{spaceBetween:20},768:{spaceBetween:30},1280:{spaceBetween:40}}});
+//# sourceMappingURL=index.d6606602.js.map
